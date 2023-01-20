81e9e9b0c1903f09e19aceb9580dc9a5f10a3548
Divy Dwivedi <divydwivedi@gmail.com>
2023-01-20 14:15:08 +0530

    included the api response to the cache after converting the byte stream through a parser

--- app/src/main/java/com/nexuscoding/stockmarketapp/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/nexuscoding/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.nexuscoding.stockmarketapp.data.repository
 
+import com.nexuscoding.stockmarketapp.data.csv.CSVParser
+import com.nexuscoding.stockmarketapp.data.csv.CompanyListingParser
 import com.nexuscoding.stockmarketapp.data.local.StockDatabase
 import com.nexuscoding.stockmarketapp.data.mapper.toCompanyListing
+import com.nexuscoding.stockmarketapp.data.mapper.toCompanyListingEntity
 import com.nexuscoding.stockmarketapp.data.remote.StockApi
 import com.nexuscoding.stockmarketapp.domain.model.CompanyListing
 import com.nexuscoding.stockmarketapp.domain.repository.StockRepository
@@ -16,7 +19,8 @@ import javax.inject.Singleton
 @Singleton
 class StockRepositoryImpl @Inject constructor(
     val api: StockApi,
-    val db: StockDatabase
+    val db: StockDatabase,
+    val companyListingsParser: CSVParser<CompanyListing>
 ): StockRepository {
 
     private val dao = db.dao
@@ -51,14 +55,36 @@ class StockRepositoryImpl @Inject constructor(
 
             val remoteListings = try {
                 val response = api.getListings()
+                companyListingsParser.parse(response.byteStream())
             } catch (e: IOException) {
                 e.printStackTrace()
                 emit(Resource.Error("Could not load data"))
+                null
             } catch (e: HttpException) {
                 e.printStackTrace()
                 emit(Resource.Error("Could not load data"))
+                null
             }
 
+            /**
+             * Following the single source of truth principle (SST) we first make an API call
+             * clear the database from the old values
+             * insert the new/updated values and then emit the data to UI from the the database
+             * so that there is only one single source of data (API)
+             */
+
+            remoteListings?.let { listings ->
+                dao.clearCompanyListings()
+                dao.insertCompanyListings(
+                    listings.map { it.toCompanyListingEntity() }
+                )
+                emit(Resource.Success(
+                    data = dao
+                        .searchCompanyListing("")
+                        .map { it.toCompanyListing() }
+                ))
+                emit(Resource.Loading(false))
+            }
         }
     }
 }
